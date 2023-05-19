4fe1380090b95c0d0a488ad9611026f6f54507a3
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2023-05-19 21:30:27 +0300

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,50 +1,96 @@
 import React, { useState, useEffect } from 'react';
+import sqlite3 from 'sqlite3';
 
 function Dashboard() {
-  const [data, setData] = useState([]);
+  const [deposits, setDeposits] = useState([]);
+  const [loans, setLoans] = useState([]);
 
   useEffect(() => {
-    // TODO: Fetch data from MongoDB database and update 'data' state
-    const fetchData = async () => {
-      try {
-        // Make API call to fetch data from the database
-        const response = await fetch('/api/data'); // Replace '/api/data' with your actual API endpoint
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    // Fetch data from the SQLite3 database
+    const db = new sqlite3.Database('expressdb.sqlite');
 
-    fetchData();
+    // Retrieve the data from the deposits table
+    db.all('SELECT * FROM deposits', (err, rows) => {
+      if (err) {
+        console.error('Error fetching deposits:', err);
+      } else {
+        setDeposits(rows);
+      }
+    });
+
+    // Retrieve the data from the loans table
+    db.all('SELECT * FROM loans', (err, rows) => {
+      if (err) {
+        console.error('Error fetching loans:', err);
+      } else {
+        setLoans(rows);
+      }
+    });
+
+    // Close the database connection
+    db.close();
   }, []);
 
   return (
     <div>
       <h1>Dashboard Page</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Amount</th>
-            <th>Date</th>
-            <th>Notes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.amount}</td>
-              <td>{item.date}</td>
-              <td>{item.notes}</td>
+
+      <div className="table-container">
+        <h2>Deposits</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Date</th>
+              <th>Notes</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {deposits.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.amount}</td>
+                <td>{item.date}</td>
+                <td>{item.notes}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="table-container">
+        <h2>Loans</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Date</th>
+              <th>Notes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {loans.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.amount}</td>
+                <td>{item.date}</td>
+                <td>{item.notes}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="table-container">
+        <h2>New Clients</h2>
+        <table>
+          {/* Render the new clients data */}
+        </table>
+      </div>
     </div>
   );
 }
 
 export default Dashboard;
-
